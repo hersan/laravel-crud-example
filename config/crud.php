c820b2d7b344963f2622f9d082f6141a332aeb81
Herminio Heredia <herminio.heredia@hotmail.com>
2018-07-27 16:43:48 -0500

    Adding config file

--- config/crud.php
+++ config/crud.php
@@ -1,1 +1,5 @@
 <?php
+
+return [
+    'authentication' => true,
+];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('web')
+Route::middleware(['web', 'auth'])
     ->namespace('Hersan\CrudExample\Http\Controllers')
     ->group(function(){
         Route::resource('users', 'UserController');
